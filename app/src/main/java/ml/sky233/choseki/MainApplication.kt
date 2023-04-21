ed0e01f525b06c58b5b24a1c00ac85dcc2533242
Sky233ml <sky233@sky233.ml>
2023-04-21 18:32:58 +0800

    配置MainApplication

--- app/src/main/java/ml/sky233/choseki/MainApplication.kt
+++ app/src/main/java/ml/sky233/choseki/MainApplication.kt
@@ -1,5 +1,18 @@
 package ml.sky233.choseki
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
 
-class MainApplication : Application()
+@SuppressLint("StaticFieldLeak")
+class MainApplication : Application() {
+    companion object {
+        lateinit var context: Context
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
