48fa9a6a1722055eb05753fa3eb7abab11e36470
Sky233ml <sky233@sky233.ml>
2023-04-29 07:33:23 +0800

    移动MainApplication并初始化PlayService和CrashHandler

---
+++ app/src/main/java/ml/sky233/choseki/application/MainApplication.kt
@@ -0,0 +1,37 @@
+package ml.sky233.choseki.application
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import ml.sky233.choseki.handler.CrashHandler
+import ml.sky233.choseki.manager.PlayerManager
+import ml.sky233.choseki.service.PlayerService
+
+@SuppressLint("StaticFieldLeak")
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var context: Context
+        lateinit var application: MainApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        application = this
+        CrashHandler.instance.init(this)
+        initService()
+    }
+
+    private fun initService() {
+        val intent = Intent(application, PlayerService::class.java)
+        application.bindService(
+            intent, PlayerManager.instance.mServiceConnection, BIND_AUTO_CREATE
+        )
+    }
+
+}
